59708cde1c4c9d3fb2c65063acfe274cd8df10ee
marie-bisbo <mariebisbo1908@gmail.com>
2023-12-26 10:23:35 +0000

    Add more plugins

--- lua/source/packer.lua
+++ lua/source/packer.lua
@@ -3,16 +3,19 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
+-- Packer
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+-- Telescope
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+-- Colourscheme
   use({
 	  'AlexvZyl/nordic.nvim',
 	  as = 'nordic',
@@ -20,5 +23,18 @@ return require('packer').startup(function(use)
 		  vim.cmd('colorscheme nordic')
 	  end
   })
+
+-- Treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+-- Harpoon
+  use('nvim-lua/plenary.nvim')
+  use('ThePrimeagen/harpoon')
+
+-- Undotree
+  use('mbbill/undotree')
+
+-- Fugitive
+  use('tpope/vim-fugitive')
 
 end)
